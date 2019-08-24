9fa033e9933c20cfb10bdd3479864c7ab5c24ec5
fengzhizi715 <fengzhizi715@126.com>
2019-08-25 01:10:39 +0800

    update EventBus

--- lib/src/main/java/com/safframework/eventbus/EventBus.kt
+++ lib/src/main/java/com/safframework/eventbus/EventBus.kt
@@ -2,6 +2,7 @@ package com.safframework.eventbus
 
 import android.util.Log
 import kotlinx.coroutines.*
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.CoroutineContext
 
 /**
@@ -20,7 +21,7 @@ object EventBus: CoroutineScope {
 
     override val coroutineContext: CoroutineContext = Dispatchers.Default + job
 
-    private val contextMap = mutableMapOf<String, MutableMap<Class<*>, EventData<*>>>()
+    private val contextMap = ConcurrentHashMap<String, MutableMap<Class<*>, EventData<*>>>()
 
     @JvmStatic
     fun <T> registerEvent(
@@ -29,23 +30,23 @@ object EventBus: CoroutineScope {
         eventClass: Class<T>,
         eventCallback: (T) -> Unit
     ) {
-        val pipeList = if (contextMap.containsKey(contextName)) {
+        val eventDataMap = if (contextMap.containsKey(contextName)) {
             contextMap[contextName]!!
         } else {
-            val eventPipe = mutableMapOf<Class<*>, EventData<*>>()
-            contextMap[contextName] = eventPipe
-            eventPipe
+            val eventDataMap = mutableMapOf<Class<*>, EventData<*>>()
+            contextMap[contextName] = eventDataMap
+            eventDataMap
         }
-        val eventData = EventData(this, eventDispatcher, eventCallback)
-        pipeList[eventClass] = eventData
+
+        eventDataMap[eventClass] = EventData(this, eventDispatcher, eventCallback)
     }
 
     @JvmStatic
-    fun send(event: Any, delaySend: Long = 0) {
+    fun send(event: Any, delayTime: Long = 0) {
 
-        if (delaySend > 0) {
+        if (delayTime > 0) {
             launch {
-                delay(delaySend)
+                delay(delayTime)
                 send(event)
             }
         } else {
@@ -57,10 +58,13 @@ object EventBus: CoroutineScope {
     fun unregisterAllEvents() {
 
         Log.i(TAG,"unregisterAllEvents()")
+
         coroutineContext.cancelChildren()
-        for ((_, pipe) in contextMap) {
-            pipe.values.forEach { it.cancel() }
-            pipe.clear()
+        for ((_, eventDataMap) in contextMap) {
+            eventDataMap.values.forEach {
+                it.cancel()
+            }
+            eventDataMap.clear()
         }
         contextMap.clear()
     }
@@ -68,19 +72,22 @@ object EventBus: CoroutineScope {
     @JvmStatic
     fun unregisterByContext(contextName: String) {
 
-        Log.i(TAG,"unregisterByContext(context: $contextName)")
+        Log.i(TAG,"$contextName")
 
-        val cloneContextList = mutableMapOf<String, MutableMap<Class<*>, EventData<*>>>()
-        cloneContextList.putAll(contextMap)
-        val pipesInContext = cloneContextList.filter { it.key == contextName }
-        for ((_, pipe) in pipesInContext) {
-            pipe.values.forEach { it.cancel() }
-            pipe.clear()
+        val cloneContexMap = ConcurrentHashMap<String, MutableMap<Class<*>, EventData<*>>>()
+        cloneContexMap.putAll(contextMap)
+        val map = cloneContexMap.filter { it.key == contextName }
+        for ((_, eventDataMap) in map) {
+            eventDataMap.values.forEach {
+                it.cancel()
+            }
+            eventDataMap.clear()
         }
         contextMap.remove(contextName)
     }
 
     private fun send(event: Any) {
+        
         val cloneContextList = mutableMapOf<String, MutableMap<Class<*>, EventData<*>>>()
         cloneContextList.putAll(contextMap)
         for ((_, pipe) in cloneContextList) {
